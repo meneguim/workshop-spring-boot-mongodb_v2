f507464e710e8e6159fc03e7c1cd18439211683f
Alan Meneguim <alan.meneguim@gmail.com>
2019-08-25 17:28:19 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/meneguim/workshopmongo/config/Instantiation.java
+++ src/main/java/com/meneguim/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.meneguim.workshopmongo.domain.Post;
 import com.meneguim.workshopmongo.domain.User;
+import com.meneguim.workshopmongo.dto.AuthorDTO;
 import com.meneguim.workshopmongo.repository.PostRepository;
 import com.meneguim.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User alan = new User(null, "Alan Meneguim", "alan.meneguim@gmail.com");		
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRespository.saveAll(Arrays.asList(maria,alex,alan));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
