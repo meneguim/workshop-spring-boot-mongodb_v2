aff530883aa0b81944dc47d2429acf2c8200ae53
Alan Meneguim <alan.meneguim@gmail.com>
2019-08-25 16:04:00 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/meneguim/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.meneguim.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.meneguim.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler  {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandarError err = new StandarError(System.currentTimeMillis(),	status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
